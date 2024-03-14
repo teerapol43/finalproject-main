9f1795fceaabf1cac1fd3d9271b00b69b80a857e
teerapol43 <teerapol.ph@rmuti.ac.th>
2024-03-14 08:27:12 +0700

    3.14.24 8.27

--- client/src/components/functions/admin.js
+++ client/src/components/functions/admin.js
@@ -1,17 +1,50 @@
 import axios from "axios";
 
-export const updateStatusOrder = async (authtoken, orderId, orderstatus) =>
-    await axios.put(process.env.REACT_APP_API + "/admin/order-status",
-        { orderId, orderstatus },
-        {
-            headers: {
-                authtoken
-            }
-        })
+// Update Status Order
+export const updateStatusOrder = async (authtoken, orderId, orderstatus) => {
+  return await axios.put(
+    process.env.REACT_APP_API + "/admin/order-status",
+    { orderId, orderstatus },
+    {
+      headers: {
+        authtoken,
+      },
+    }
+  );
+};
+
+// Get Orders for Admin
 export const getOrdersAdmin = async (authtoken) => {
-    return await axios.get(process.env.REACT_APP_API + "/admin/orders", {
-        headers: {
-            authtoken,
-        },
-    });
+  return await axios.get(process.env.REACT_APP_API + "/admin/orders", {
+    headers: {
+      authtoken,
+    },
+  });
+};
+
+// Get Edit User
+export const getEditUser = async (authtoken) => {
+  return await axios.get(process.env.REACT_APP_API + "/edit-user-details", {
+    headers: {
+      authtoken,
+    },
+  });
+};
+
+// Get Edit Order
+export const getEditOrder = async (authtoken) => {
+  return await axios.get(process.env.REACT_APP_API + "/edit-order-details", {
+    headers: {
+      authtoken,
+    },
+  });
+};
+
+// Get Edit Product
+export const getEditProduct = async (authtoken) => {
+  return await axios.get(process.env.REACT_APP_API + "/edit-product-details", {
+    headers: {
+      authtoken,
+    },
+  });
 };
